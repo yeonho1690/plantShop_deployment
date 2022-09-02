c67a75874c1c36da98fe75509c22f679fdb269ee
ghkwns5191 <ghkwns5191@gmail.com>
2022-09-02 19:26:50 +0900

    공지사항 &  배송지관리 Controller

---
+++ demo/src/main/java/com/example/demo/controller/AddressController.java
@@ -0,0 +1,134 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Address;
+import com.example.demo.repository.AddressRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.AddressService;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Controller
+public class AddressController {
+	
+//	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
+//	UserDetails userDetails = (UserDetails)principal; 
+//	String username = ((UserDetails) principal).getUsername(); 
+//	String password = ((UserDetails) principal).getPassword();
+	
+	@Autowired
+	private final AddressRepository addressRepository;
+	@Autowired
+	private final UserRepository userRepository;
+	
+	@Autowired
+	private final AddressService addressService;
+	
+	
+	
+	
+	@PostMapping("/auth/address/add")
+	public ResponseEntity<Address> registerAddress( @RequestBody Address address){
+		try {
+			
+			
+			Address _address = this.addressRepository.save(new Address(address.getAname(), address.getAreceipt(), address.getAmobile(), address.getApostcode(), address.getAaddress1(), address.getAaddress2(), address.getUsername()));
+		
+			return new ResponseEntity<>(_address, HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	
+	@GetMapping("/auth/address/list/{username}")
+	public ResponseEntity<List<Address>> addressList(@RequestParam(required = false) String aname, @PathVariable("username") String username){
+		try {
+			
+			List<Address> address = new ArrayList<Address>();
+			
+				addressRepository.findAllByUsername(username).forEach(address::add);
+			  if(address.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			} 
+			return new ResponseEntity<>(address, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	} 
+	
+	@GetMapping("/auth/address/modify/{aid}")
+	public ResponseEntity<Address> addressDetail(@PathVariable("aid") Integer aid){
+		Optional<Address> address = addressRepository.findById(aid);
+		if(address.isPresent()) {
+			address.get().setAname(address.get().getAname());
+			address.get().setAreceipt(address.get().getAreceipt());
+			address.get().setAmobile(address.get().getAmobile());
+			address.get().setApostcode(address.get().getApostcode());
+			address.get().setAaddress1(address.get().getAaddress1());
+			address.get().setAaddress2(address.get().getAaddress2());
+			return new ResponseEntity<>(address.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/auth/address/modify/{aid}")
+	public ResponseEntity<Address> addressDetail(@PathVariable("aid") Integer aid, @RequestBody Address address){
+		Optional<Address> addressData = addressRepository.findById(aid);
+		Address newAddress = addressData.get();
+		if(addressData.isPresent()) {
+			newAddress.setAname(address.getAname());
+			newAddress.setAreceipt(address.getAreceipt());
+			newAddress.setAmobile(address.getAmobile());
+			newAddress.setApostcode(address.getApostcode());
+			newAddress.setAaddress1(address.getAaddress1());
+			newAddress.setAaddress2(address.getAaddress2());
+			return new ResponseEntity<>(addressRepository.save(newAddress), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/auth/address/delete/{aid}")
+	public ResponseEntity<HttpStatus> addressDelete(@PathVariable("aid") Integer aid){
+		try {
+			addressRepository.deleteById(aid);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	
+	
+	
+
+	
+	
+}
---
+++ demo/src/main/java/com/example/demo/controller/NoticeController.java
@@ -0,0 +1,140 @@
+package com.example.demo.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Notice;
+import com.example.demo.repository.NoticeRepository;
+import com.example.demo.service.NoticeService;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Controller
+public class NoticeController {
+
+	@Autowired
+	private final NoticeService noticeService;
+	private final NoticeRepository noticeRepository;
+//	private final MemberService memberService;
+
+	@GetMapping("/auth/notice/list")
+	public ResponseEntity<List<Notice>> list(@RequestParam(required = false) String ntitle) {
+		try {
+			List<Notice> notice = new ArrayList<Notice>();
+			if (ntitle == null)
+				noticeRepository.findAll().forEach(notice::add);
+			else
+				noticeRepository.findByNtitleContaining(ntitle).forEach(notice::add);
+			if (notice.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(notice, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/auth/notice/detail_admin/{nid}")
+	public ResponseEntity<Notice> noticeDetailAdmin(Model model, @PathVariable("nid") Integer nid) {
+		Notice notice = this.noticeService.getNotice(nid);
+		model.addAttribute("notice", notice);
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
+
+	@GetMapping("/auth/notice/detail/{nid}")
+	public ResponseEntity<Notice> noticeDetail(@PathVariable("nid") Integer nid) {
+		Optional<Notice> noticeData = noticeRepository.findById(nid);
+		noticeData.get().setNhit(noticeData.get().getNhit() + 1);
+		noticeRepository.save(noticeData.get());
+		if (noticeData.isPresent()) {
+			return new ResponseEntity<>(noticeData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+//	@GetMapping("/register")
+//	public String registerNotice() {
+//		return "noticeadd_admin";
+//	}
+
+	@PostMapping("/auth/notice/register")
+	public ResponseEntity<Notice> registerNotice(@RequestBody Notice notice) {
+
+		try {
+
+			Notice _notice = this.noticeRepository.save(
+					new Notice(notice.getNtitle(), notice.getNcontent(), notice.getNfile(), LocalDate.now(), null, 0));
+			return new ResponseEntity<>(_notice, HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@GetMapping("/auth/notice/modify/{nid}")
+	public ResponseEntity<Notice> noticeModify(@PathVariable("nid") Integer nid
+			) {
+		try {
+		Optional<Notice> notice = this.noticeRepository.findById(nid); 
+		notice.get().setNtitle(notice.get().getNtitle());
+		notice.get().setNcontent(notice.get().getNcontent());
+		notice.get().setNrdate(notice.get().getNrdate());
+		notice.get().setNmodifydate(notice.get().getNmodifydate());
+		notice.get().setNhit(notice.get().getNhit());
+		return new ResponseEntity<>(notice.get(), HttpStatus.OK);
+		} catch(Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/auth/notice/modify/{nid}")
+	public ResponseEntity<Notice> noticeModify(@PathVariable("nid") Integer nid, @RequestBody Notice notice) {
+		
+			Optional<Notice> noticeData = noticeRepository.findById(nid);
+			if(noticeData.isPresent()) {
+				Notice newNotice = noticeData.get();
+				newNotice.setNtitle(notice.getNtitle());
+				newNotice.setNcontent(notice.getNcontent());
+				newNotice.setNrdate(noticeData.get().getNrdate());
+				newNotice.setNmodifydate(LocalDate.now());
+				newNotice.setNfile(notice.getNfile());
+				newNotice.setNhit(noticeData.get().getNhit());
+				return new ResponseEntity<>(noticeRepository.save(newNotice), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		
+	}
+
+	@DeleteMapping("/auth/notice/delete/{nid}")
+	public ResponseEntity<HttpStatus> noticeDelete(@PathVariable("nid") Integer nid) {
+		try {
+			noticeRepository.deleteById(nid);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
