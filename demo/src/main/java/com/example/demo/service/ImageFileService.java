385bee71f5c68ed2863e0a3419aaf1609d0a9d3c
naahnoey <naahyin20@sookmyung.ac.kr>
2022-09-03 10:55:39 +0900

    상품 이미지 등록 및 조회(이미지 정보 제외)

---
+++ demo/src/main/java/com/example/demo/service/ImageFileService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.model.ImageFile;
+import com.example.demo.repository.ImageFileRepository;
+
+@Service
+public class ImageFileService {
+
+	@Autowired
+	ImageFileRepository fileRepository;
+	
+	public ImageFile save(MultipartFile file) throws IOException {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		
+		ImageFile imageFile = new ImageFile(fileName, file.getContentType(), file.getBytes());
+		
+		return fileRepository.save(imageFile);
+	}
+	
+	public ImageFile getFile(Integer fid) {
+		return fileRepository.findById(fid).get();
+	}
+	public Stream<ImageFile> getAllFiles() {
+		return fileRepository.findAll().stream();
+	}
+	
+}
--- demo/src/main/java/com/example/demo/service/ProductService.java
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.ImageFile;
 import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
 
@@ -15,13 +16,13 @@ public class ProductService {
 	@Autowired
 	private final ProductRepository productRepository;
 	
-	public Product register(String pname, String ptype, Integer pprice, Integer pstock, String pImg1, String pdetail ) {
+	public Product register(String pname, String ptype, Integer pprice, Integer pstock, Integer pimg1, String pdetail ) {
 		Product product = new Product();
 		product.setPname(pname);
 		product.setPtype(ptype);
 		product.setPprice(pprice);
 		product.setPstock(pstock);
-		product.setPImg1(pImg1);
+		product.setPimg1(pimg1);
 		product.setPdetail(pdetail);
 		return this.productRepository.save(product);
 		
