4d8f42f5fff2a5ae685666063adbf6c7843cb051
hwajin-jo <wa8784@naver.com>
2022-09-13 11:14:21 +0900

    product controller 갱신

--- demo/src/main/java/com/example/demo/controller/ProductControll.java
+++ demo/src/main/java/com/example/demo/controller/ProductControll.java
@@ -32,107 +32,117 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 @Controller
-public class ProductControll {
+public class ProductController {
 
-	@Autowired
-	private final ProductService productService;
-	
-	private final ProductRepository productRepository;
-	
-	@Autowired
-	ImageFileService fileService;
+   @Autowired
+   private final ProductService productService;
+   
+   private final ProductRepository productRepository;
+   
+   @Autowired
+   ImageFileService fileService;
 
-	
-	// 목록
-	@GetMapping("/{ptype}")
-	public ResponseEntity<List<Product>> productList(@PathVariable("ptype") String ptype) {
-		try {
-			List<Product> products = new ArrayList<Product>();
-			
-			if (ptype.equals("all"))
-				productRepository.findAll().forEach(products::add);
-			else
-				productRepository.findByPtype(ptype).forEach(products::add);
-			
-			if(products.isEmpty())
-				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-			
-			return new ResponseEntity<>(products, HttpStatus.OK);
-		} catch(Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	// 관리자 상품관리 리스트
-	@GetMapping("/all")
-	public ResponseEntity<List<Product>> productAdminList() {
-		try {
-			List<Product> products = new ArrayList<Product>();
-			
-			productRepository.findAll().forEach(products::add);
-			
-			return new ResponseEntity<>(products, HttpStatus.OK);
-		} catch(Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	// 상세페이지 
-	@GetMapping("/detail/{pid}")
-	public ResponseEntity<Product> productDetail (@PathVariable("pid") Long pid) {
-		Optional<Product> productData = productRepository.findById(pid);
-		productRepository.save(productData.get());
-		if(productData.isPresent()) {
-			return new ResponseEntity<>(productData.get(), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	@GetMapping("/files")
-	public ResponseEntity<List<ResponseFile>> getListFiles() {
-		
-		List<ResponseFile> files = fileService.getAllFiles().map(dbFile -> {
-			String fileDownloadUri = ServletUriComponentsBuilder
-					.fromCurrentContextPath()
-					.path("/api/product/files/")
-					.path(dbFile.getFid())
-					.toUriString();
-			
-			return new ResponseFile(
-					dbFile.getName(), 
-					fileDownloadUri, 
-					dbFile.getType(), 
-					dbFile.getData().length);
-		}).collect(Collectors.toList());
-		return ResponseEntity.status(HttpStatus.OK).body(files);
-	}
-	
-
-	@GetMapping("/files/{fid}")
-	public ResponseEntity<byte[]> getFile(@PathVariable String fid) {
-		ImageFile file = fileService.getFile(fid);
-		
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
-				.body(file.getData());
-	}
-	
-	// 등록
-	@PostMapping("/register")
-	public ResponseEntity<Product> registerProduct(@RequestPart(value = "pdata") Product product, @RequestPart(value = "pfile") MultipartFile file) {
-		try {
-			ImageFile imageFile = fileService.save(file);	// �信�� �޾ƿ� �̹��� ���� ����
-			
-			// 이미지 포함 상품 정보 저장
-			Product _product = this.productRepository.save(
-					new Product(product.getPname(), product.getPtype(), product.getPprice(), product.getPstock(), imageFile.getFid(), product.getPdetail()));
-			return new ResponseEntity<>(_product, HttpStatus.CREATED);
-					
-		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
+   
+   // 목록
+   @GetMapping("/{ptype}")
+   public ResponseEntity<List<Product>> productList(@PathVariable("ptype") String ptype) {
+      try {
+         List<Product> products = new ArrayList<Product>();
+         
+         if (ptype.equals("all"))
+            productRepository.findAll().forEach(products::add);
+         else
+            productRepository.findByPtype(ptype).forEach(products::add);
+         
+         if(products.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+         
+         return new ResponseEntity<>(products, HttpStatus.OK);
+      } catch(Exception e) {
+         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+   }
+   
+   // 상세정보
+   @GetMapping("/detail/{pid}")
+   public ResponseEntity<Product> productDetail (@PathVariable("pid") Long pid) {
+      Optional<Product> productData = productRepository.findById(pid);
+      productRepository.save(productData.get());
+      if(productData.isPresent()) {
+         return new ResponseEntity<>(productData.get(), HttpStatus.OK);
+      } else {
+         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
+   }
+   
+//   // 전체 이미지 파일
+//   @GetMapping("/files")
+//   public ResponseEntity<List<ResponseFile>> getListFiles() {
+//      
+//      List<ResponseFile> files = fileService.getAllFiles().map(dbFile -> {
+//         String fileDownloadUri = ServletUriComponentsBuilder
+//               .fromCurrentContextPath()
+//               .path("/api/product/file/")
+//               .path(dbFile.getFid())
+//               .toUriString();
+//         
+//         return new ResponseFile(
+//               dbFile.getName(), 
+//               fileDownloadUri, 
+//               dbFile.getType(), 
+//               dbFile.getData().length);
+//      }).collect(Collectors.toList());
+//      return ResponseEntity.status(HttpStatus.OK).body(files);
+//   }
+//   // 특정 이미지 파일의 ResponseFile
+//   @GetMapping("/files/{id}")
+//   public ResponseEntity<ResponseFile> getResponseFile(@PathVariable String id) {
+//      ImageFile dbFile = fileService.getFile(id);
+//      String fileDownloadUri = ServletUriComponentsBuilder
+//            .fromCurrentContextPath()
+//            .path("/api/product/file/")
+//            .path(dbFile.getFid())
+//            .toUriString();
+//      ResponseFile file = new ResponseFile(
+//                           dbFile.getName(), 
+//                           fileDownloadUri, 
+//                           dbFile.getType(), 
+//                           dbFile.getData().length
+//                        );
+//      return new ResponseEntity<>(file, HttpStatus.OK);
+//   }
+   
+   // 특정 이미지 파일
+   @GetMapping("/file/{id}")
+   public ResponseEntity<byte[]> getFile(@PathVariable String id) {
+      ImageFile file = fileService.getFile(id);
+      
+      return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+            .body(file.getData());
+   }
+   
+   // 등록
+   @PostMapping("/register")
+   public ResponseEntity<Product> registerProduct(@RequestPart(value = "pdata") Product product, @RequestPart(value = "pfile") MultipartFile file) {
+      try {
+         ImageFile imageFile = fileService.save(file);   // 이미지 파일 아이디
+         
+         // 이미지 파일 경로
+         String fileDownloadUri = ServletUriComponentsBuilder
+               .fromCurrentContextPath()
+               .path("/api/product/file/")
+               .path(imageFile.getFid())
+               .toUriString();
+         
+         // 이미지 포함 상품 정보 저장
+         Product _product = this.productRepository.save(
+               new Product(product.getPname(), product.getPtype(), product.getPprice(), product.getPstock(), fileDownloadUri, product.getPdetail()));
+         return new ResponseEntity<>(_product, HttpStatus.CREATED);
+               
+      } catch (Exception e) {
+         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+   }
+   
 }
-
