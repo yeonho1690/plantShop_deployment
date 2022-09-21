421922686b448a781538dcc8b28e2f2aeca97eca
yeonho <yeonho1690@naver.com>
2022-09-21 12:20:13 +0900

    1

--- demo/src/frontend/src/views/ChatbotView.vue
+++ demo/src/frontend/src/views/ChatbotView.vue
@@ -2,7 +2,7 @@
    <section>
       <div id="a1">
          <img src="../assets/chatbotstart.png" id="image"><br>
-         <textarea id="start" v-model="mytext"/><br>
+         <!-- <textarea id="start" v-model="mytext"/><br> -->
          <pre>{{mytext}}더 많은 식물 관련 정보를 얻고 싶으시면<br><a :href="chatbot.url">{{chatbot.title}}</a>에 방문해 주세요</pre><br>
          <textarea id="content" rows=20></textarea><br>                
          <input type="text" id="queryin"> 
@@ -60,7 +60,7 @@
 <style>
 #a1{
    margin: auto;
-   text-align: left;
+   text-align: center;
    margin-top: 50px;
    margin-bottom: 150px;
 }
